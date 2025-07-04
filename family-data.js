4b30e06edd01ecec74e9b3df66ff854b16976d8e
HM <129320543+himanshumattoo@users.noreply.github.com>
2025-07-04 16:10:00 -0600

    Update family-data.js

--- family-data.js
+++ family-data.js
@@ -6,19 +6,65 @@ const familyConfig = {
     node: { collapsable: true }
   },
   nodeStructure: {
-    text: { name: "Pandit Shambhu Kachroo" },
+    text: { name: "Pandit Shambhu Kachroo\n& Sobhagyawati" },
     children: [
       {
-        text: { name: "Uday Chandan Kachroo" },
+        text: { name: "Uday Chandan Kachroo\n+ Lalita" },
         children: [
-          { text: { name: "Tej Bahadur K." } },
-          { text: { name: "Prakash (Beema)" } }
+          { text: { name: "Tej Bahadur K.\n+ Beena" } },
+          { text: { name: "Prakash (Beema)\n+ Ashok Tickoo" } },
+          { text: { name: "Ravinder K.\n+ Nimmi" } },
+          { text: { name: "Indra (Jiji)\n+ Rashinder Trisal" } },
+          { text: { name: "Savita Narayan K.\n+ Jeenak" } }
         ]
       },
       {
-        text: { name: "Naginder Mohan Kachroo" },
+        text: { name: "Naginder Mohan Kachroo\n+ Rani" },
         children: [
-          { text: { name: "Romesh (Sabtoth)" } }
+          { text: { name: "Romesh (Sabtoth)\n+ Irene" } },
+          { text: { name: "Vijay (Beenua)" } },
+          { text: { name: "Bal Krishan (BK)" } }
+        ]
+      },
+      {
+        text: { name: "Rajdulari (Dida)\n+ Trilok Moza" },
+        children: [
+          { text: { name: "Kanaya Lal Moza\n+ Veera" } },
+          { text: { name: "Asha Kaul\n+ Naresh" } },
+          { text: { name: "Virender Moza (Veerji)\n+ Vijanti" } },
+          { text: { name: "Meenakshi Moza (Kuku)" } }
+        ]
+      },
+      {
+        text: { name: "Sushil (Bengashi)\n+ Hidenath Pakmoo" },
+        children: [
+          { text: { name: "Renu Challu (Bala)\n+ Vijay Challu" } },
+          { text: { name: "Rita Parimoo (Billi)" } },
+          { text: { name: "Reeva Parimoo\n+ Rakesh Raina" } }
+        ]
+      },
+      {
+        text: { name: "Janwanti (Jygree)\n+ Omkar Nath Dhar" },
+        children: [
+          { text: { name: "Sarojni Sarla Kaul\n+ Deepak" } },
+          { text: { name: "Shashi Kaul\n+ Ramesh Kaul" } },
+          { text: { name: "Sudhir Dhar (Shaadi)\n+ Anita" } },
+          { text: { name: "Sujata Mattoo\n+ Tej K." } }
+        ]
+      },
+      {
+        text: { name: "Dilaram Kachroo (Bobuji)\n+ Mohini" },
+        children: [
+          { text: { name: "Leela Dhar K." } },
+          { text: { name: "Gayatri" } },
+          { text: { name: "Vidya Dhar K.\n+ Andrea" } }
+        ]
+      },
+      {
+        text: { name: "Ishwer Chander Kachroo\n+ Pitabi" },
+        children: [
+          { text: { name: "Suman K.\n+ Ray Hoffpfieur" } },
+          { text: { name: "Sachin K. (Raja)\n+ Pooja" } }
         ]
       }
     ]

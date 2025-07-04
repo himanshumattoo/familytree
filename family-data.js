92037b515e493baed30135c6fdddeac081b9175b
HM <129320543+himanshumattoo@users.noreply.github.com>
2025-07-04 15:57:39 -0600

    Update family-data.js

--- family-data.js
+++ family-data.js
@@ -1,1 +1,28 @@
+const familyConfig = {
+  chart: {
+    container: "#tree-simple",
+    rootOrientation: "NORTH",
+    connectors: { type: "step" },
+    node: { collapsable: true }
+  },
+  nodeStructure: {
+    text: { name: "Pandit Shambhu Kachroo" },
+    children: [
+      {
+        text: { name: "Uday Chandan Kachroo" },
+        children: [
+          { text: { name: "Tej Bahadur K." } },
+          { text: { name: "Prakash (Beema)" } }
+        ]
+      },
+      {
+        text: { name: "Naginder Mohan Kachroo" },
+        children: [
+          { text: { name: "Romesh (Sabtoth)" } }
+        ]
+      }
+    ]
+  }
+};
 
+new Treant(familyConfig);
